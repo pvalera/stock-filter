c3b8b11a42bc14164ea259ca1938db4258af4321
unknown <pvalera@hotmail.com>
2020-05-31 19:25:08 -0700

    Code streams to filter stocks

--- src/MakeMeRich.java
+++ src/MakeMeRich.java
@@ -1,22 +1,34 @@
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MakeMeRich {
 	public static final List<String> symbols = Arrays.asList("AMD", "HPQ",
 			"IBM", "TXN", "VMW", "XRX", "AAPL", "ADBE", "AMZN", "FB", "CSCO",
-			"SNE", "GOOG", "INTC", "INTU", "MSFT", "ORCL", "BABA", "VRSN");
+			"SNE", "GOOG", "INTC", "INTU", "MSFT", "ORCL", "BABA", "VRSN","BTC-USD");
 
 	public static void main(String[] args) {
-
+		
 		// 1. Print these symbols using a Java 8 for-each and lambdas
-
+		Stream<Object> stockSymbol = symbols.stream().map(String -> String);
+		System.out.println("Print stock symbols:");
+		stockSymbol.forEach(System.out::println);
+		
 		// 2. Use the StockUtil class to print the price of Bitcoin
-
+		System.out.println("Price of Bitcoin: " + StockUtil.getPrice("BTC-USD"));
+		
 		// 3. Create a new List of StockInfo that includes the stock price
+		List<StockInfo> stockInfoList = symbols.stream().map(symbol -> StockUtil.getPrice(symbol)).collect(Collectors.toList());
+		System.out.println("New List of StockInfo with the stock price: ");
+		stockInfoList.forEach(System.out::println);
 	
 		// 4. Find the highest-priced stock under $500
-	
-	}
+		StockInfo symbolStream = stockInfoList.stream()
+				.filter(StockUtil.isPriceLessThan(500))
+				.sorted(((s1, s2) -> Double.compare(s1.price, s2.price))).reduce((a, b) -> b).orElse(null);
+		System.out.println("The highest priced stock under $500: " + symbolStream);
 
+	}
 }
