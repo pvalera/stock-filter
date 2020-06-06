702e64a4fa62ce089606d67741f85eb71bbb7b1a
unknown <pvalera@hotmail.com>
2020-06-05 19:49:01 -0700

    Modified in order to use the updated StockUtil method

--- src/MakeMeRich.java
+++ src/MakeMeRich.java
@@ -5,11 +5,13 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MakeMeRich {
-	public static final List<String> symbols = Arrays.asList("AMD", "HPQ",
-			"IBM", "TXN", "VMW", "XRX", "AAPL", "ADBE", "AMZN", "FB", "CSCO",
-			"SNE", "GOOG", "INTC", "INTU", "MSFT", "ORCL", "BABA", "VRSN","BTC-USD");
+//	public static final List<String> symbols = Arrays.asList("AMwD", "HPQ",
+//			"IBM", "TXN", "VMW", "XRX", "AAPL", "ADBE", "AMZN", "FB", "CSCO",
+//			"SNE", "GOOG", "INTC", "INTU", "MSFT", "ORCL", "BABA", "VRSN","BTC-USD");
+	public static final List<String> symbols = Arrays.asList("AMwD", "HPQ",
+	"IBM", "TXN","VMW");
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		// 1. Print these symbols using a Java 8 for-each and lambdas
 		Stream<Object> stockSymbol = symbols.stream().map(String -> String);
@@ -20,8 +22,16 @@ public class MakeMeRich {
 		System.out.println("Price of Bitcoin: " + StockUtil.getPrice("BTC-USD"));
 		
 		// 3. Create a new List of StockInfo that includes the stock price
-		List<StockInfo> stockInfoList = symbols.stream().map(symbol -> StockUtil.getPrice(symbol)).collect(Collectors.toList());
 		System.out.println("New List of StockInfo with the stock price: ");
+		List<StockInfo> stockInfoList = symbols.stream().map(symbol -> {
+			try {
+				return StockUtil.getPrice(symbol);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+				return null;
+			}
+		}).collect(Collectors.toList());
 		stockInfoList.forEach(System.out::println);
 	
 		// 4. Find the highest-priced stock under $500
